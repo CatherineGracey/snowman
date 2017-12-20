81a2d570ad6829d24fa3e3901904895bd25c23a1
Catherine Gracey <catherine.gracey@gmail.com>
2017-12-20 19:04:31 +1100

    Can make snow fall.

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-var fallingSnow, isSnowFalling = false;
+var fallingSnow, isSnowFalling = true;
 
 function randomNumber(max = 2){
   return Math.floor(Math.random() * max)
@@ -11,10 +11,24 @@ function makeSnow(){
   var pageWidth = window.innerWidth;
   newSnow.style.left = randomNumber(pageWidth) + "px";
   newSnow.style.top = "1px";
+  if (randomNumber()){
+    newSnow.style.zIndex = "2";
+  }
 }
 
 function makeSnowFall(){
-  makeSnow();
+  if (isSnowFalling){
+    makeSnow();
+    if (randomNumber(100) > 98){
+      isSnowFalling = false;
+    }
+  }
+  var snowFlakes = document.getElementsByClassName("snow");
+  for (var snowflake = 0; snowflake < snowFlakes.length; snowflake++){
+    var height = parseInt(snowFlakes[snowflake].style.top);
+    height++;
+    snowFlakes[snowflake].style.top = height + "px";
+  }
 }
 
 window.onload = function(){
